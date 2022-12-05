71926a1a02bb956acc0145f2d1f3e9e93a6880a8
Jayque17 <julien.leon99@gmail.com>
2022-12-05 16:23:35 +0100

    finally finished

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,21 +8,33 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'PS5',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A video game station design and built by Sony'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Switch',
+    price: 399,
+    description: 'You could play to Zelda on it.'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Personal computer',
+    price: 130,
+    description: 'Only for desktop buisness.'
+  },
+  {
+    id: 4,
+    name: 'Not Affordable Personal computer',
+    price: 4000,
+    description: 'You can do everything with that monster.'
+  },
+  {
+    id: 5,
+    name: 'An entire day with Maël and Thomas',
+    price: 15000,
+    description: 'Yeah, it is expensive but it is normal, they are so BEAUtifull'
   }
 ];
 
